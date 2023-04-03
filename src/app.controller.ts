0da6d4423f43d503a02410c65f2f094adac5d524
havidtech <76119744+havidtech@users.noreply.github.com>
2023-04-03 18:56:49 +0100

    chore: init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Post()
+  getHello() {
     return this.appService.getHello();
+  }
+
+  @Post('/videos')
+  @UseInterceptors(FileInterceptor('video', { dest: './upload' }))
+  uploadVideo(@UploadedFile() video: Express.Multer.File) {
+    return this.appService.uploadToSpheron(video);
+  }
+
+  @Post('/metadatas')
+  @UseInterceptors(FileInterceptor('metadata', { dest: './upload' }))
+  uploadMetadata(@UploadedFile() metadata: Express.Multer.File) {
+    return this.appService.uploadToSpheron(metadata);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { SpheronClient, ProtocolEnum } from '@spheron/storage';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  private spheronClient = new SpheronClient({
+    token: process.env.SPHERONTOKEN,
+  });
+
+  getHello() {
     return 'Hello World!';
+  }
+
+  async uploadToSpheron(file: Express.Multer.File) {
+    let currentlyUploaded = 0;
+    const { protocolLink } = await this.spheronClient.upload(file.path, {
+      protocol: ProtocolEnum.IPFS,
+      onUploadInitiated: (uploadId) => {
+        console.log(`Upload with id ${uploadId} started...`);
+      },
+      onChunkUploaded: (uploadedSize, totalSize) => {
+        currentlyUploaded += uploadedSize;
+        console.log(`Uploaded ${currentlyUploaded} of ${totalSize} Bytes.`);
+      },
+      name: 'fileUpload',
+    });
+
+    return {
+      link: `${protocolLink}/${file.filename}`,
+    };
   }
 }
